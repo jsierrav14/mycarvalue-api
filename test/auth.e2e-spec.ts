4b1c4815c4264c702d213c6d6251366f436f2591
Jose Sierra <jsierrav14@gmail.com>
2022-11-21 09:22:55 -0500

    more integrations test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -13,21 +13,28 @@ describe('Authentication System', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    setupApp(app)
+    setupApp(app);
     await app.init();
   });
 
   it('Handles a signup request', () => {
-
     const email = 'asd12@email.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email, password: '1234as' })
       .expect(201)
-      .then(res=>{
-        const {id,email} = res.body;
-        expect(id).toBeDefined()
-        expect(email).toBe('asd12@email.com')
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toBe('asd12@email.com');
       });
+  });
+
+  it('Sign up as a new user then get the currently logged in user', () => {
+    const email = 'asdf@asdf.com';
+    request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
   });
 });
